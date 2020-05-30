e140f7609714b034ea356193998f35fb047d7e8b
ravikumarkubusada <48872854+ravikumarkubusada@users.noreply.github.com>
2020-05-30 13:26:15 +0530

    adding code for stack impl with array

---
+++ src/stack/arrayImpl/Arraystack.java
@@ -0,0 +1,56 @@
+package stack.arrayImpl;
+
+import java.util.EmptyStackException;
+
+public class Arraystack {
+	private Employee[] stack;
+	private int top;
+
+	public Arraystack(int capacity) {
+		stack = new Employee[capacity];
+	}
+
+	public void push(Employee emp) {
+		if (stack.length == top) { // stack is full
+//			need to resize the array
+			Employee[] newStack = new Employee[2 * stack.length];
+			System.arraycopy(stack, 0, newStack, 0, stack.length); // O(n)
+			stack = newStack;
+		}
+		System.out.println("now top is at: "+top);
+		stack[top++] = emp; // O(1)
+	}
+
+	public Employee pop() {
+		if (isEmpty())
+			throw new EmptyStackException();
+
+		Employee e = stack[--top];
+		stack[top] = null;
+
+		return e;
+
+	}
+
+	public boolean isEmpty() {
+		return top == 0;
+	}
+
+	public Employee peek() {
+
+		if (isEmpty())
+			throw new EmptyStackException();
+
+		return stack[top - 1];
+	}
+
+	public int size() {
+		return top;
+	}
+
+	public void printStack() {
+		for (int i = top - 1; i >= 0; i--) {
+			System.out.println(stack[i]);
+		}
+	}
+}
--- src/stack/arrayImpl/Main.java
+++ src/stack/arrayImpl/Main.java
@@ -7,6 +7,22 @@ public class Main {
 		Employee e1 = new Employee("Ravi", 123L);
 		Employee e2 = new Employee("Raj", 124L);
 		Employee e3 = new Employee("Ram", 125L);
+		
+		Arraystack st = new Arraystack(10);
+		
+		st.push(e1);
+		st.push(e2);
+		st.push(e3);
+		
+		st.printStack();
+		System.out.println(st.size());
+//		System.out.println(st.peek());
+//		
+//		st.pop();
+		
+//		System.out.println(st.pop());
+		
+//		System.out.println(st.peek());
 	}
 
 }
